9d6caa59ee621916078a3f42ffa0a051d6190bbe
zhangqingwei <691159453@qq.com>
2023-01-08 01:21:02 +0800

    完成用户列表

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,14 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 0,
     'space-before-function-paren': 0,
-    'object-curly-spacing': 0
+    'object-curly-spacing': 0,
+    'vue/no-deprecated-slot-attribute': 0,
+    'comma-spacing': 0,
+    'quote-props': 0,
+    'vue/no-deprecated-slot-scope-attribute': 0,
+    'object-curly-newline': 0,
+    'no-multiple-empty-lines': 0,
+    'object-property-newline': 0,
+    'indent': 0
   }
 }
