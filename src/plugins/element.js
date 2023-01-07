e5c020d946ca13799075229e9325f9374b98b751
zhangqingwei <691159453@qq.com>
2023-01-08 01:21:54 +0800

    完成用户列表

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,9 @@
 import 'element-plus/lib/theme-chalk/index.css'
-import {ElForm, ElButton, ElFormItem, ElInput, ElMessage} from 'element-plus'
+import {
+  ElForm, ElButton, ElFormItem, ElInput, ElMessage,ElContainer,ElHeader,ElAside,ElMain,ElMenu,ElSubmenu,ElMenuItem,ElBreadcrumb,
+  ElBreadcrumbItem,ElCard,ElRow,ElCol,ElTable,ElTableColumn,ElSwitch,ElTooltip,ElPagination,ElDialog,ElMessageBox, ElTag, ElTree, ElSelect,
+  ElOption,Search
+} from 'element-plus'
 
 export default (app) => {
   app.use(ElForm)
@@ -7,4 +11,28 @@ export default (app) => {
   app.use(ElFormItem)
   app.use(ElInput)
   app.use(ElMessage)
+  app.use(ElContainer)
+  app.use(ElHeader)
+  app.use(ElAside)
+  app.use(ElMain)
+  app.use(ElMenu)
+  app.use(ElSubmenu)
+  app.use(ElMenuItem)
+  app.use(ElBreadcrumb)
+  app.use(ElBreadcrumbItem)
+  app.use(ElCard)
+  app.use(ElRow)
+  app.use(ElCol)
+  app.use(ElTable)
+  app.use(ElTableColumn)
+  app.use(ElSwitch)
+  app.use(ElTooltip)
+  app.use(ElPagination)
+  app.use(ElDialog)
+  app.use(ElMessageBox)
+  app.use(ElTag)
+  app.use(ElTree)
+  app.use(ElSelect)
+  app.use(ElOption)
+  app.use(Search)
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  { path: '/home', component: Home ,redirect: '/welcome',children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]}
 ]
 
 const router = createRouter({
